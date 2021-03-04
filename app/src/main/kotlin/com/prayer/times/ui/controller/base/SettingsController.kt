e759a8294bf3f00efbf93193923527e1deda2f04
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2021-03-04 00:45:21 +0000

    Rewrite VIII

--- app/src/main/kotlin/com/prayer/times/ui/controller/base/SettingsController.kt
+++ app/src/main/kotlin/com/prayer/times/ui/controller/base/SettingsController.kt
@@ -25,7 +25,7 @@ abstract class SettingsController : BaseController<PreferencesLayoutBinding>() {
         return binding.root
     }
 
-    abstract fun getSettingsList(): List<AbstractItem<RecyclerView.ViewHolder>> 
+    abstract fun getSettingsList(): List<AbstractItem<FastAdapter.ViewHolder>> 
 
     override fun onViewCreated(view: View) {
         super.onViewCreated(view)

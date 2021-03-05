d5c0e9c4f67bb52bdf3e42c0613dac913ed1e4d2
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2021-03-05 19:35:53 +0000

    Rewrite XVI

--- app/src/main/kotlin/com/prayer/times/ui/controller/settings/SettingsControllerMain.kt
+++ app/src/main/kotlin/com/prayer/times/ui/controller/settings/SettingsControllerMain.kt
@@ -1,37 +1,38 @@
 package com.prayer.times.ui.controller.settings
 
-import android.content.Context
 import com.prayer.times.R
 import com.prayer.times.ui.controller.base.SettingsController
 
 class SettingsControllerMain : SettingsController() {
 
-    private val size = 16 * resources!!.getDisplayMetrics().scaledDensity
-
-    override fun getSettingsList() = listOf(
-        SettingsItemMain(
-            resources!!.getString(R.string.settings_general),
-            R.drawable.ic_general_24dp,
-            size,
-            this
-        ),
-        SettingsItemMain(
-            resources!!.getString(R.string.settings_themes),
-            R.drawable.ic_theme_24dp,
-            size,
-            this
-        ),
-        SettingsItemMain(
-            resources!!.getString(R.string.settings_relocate),
-            R.drawable.ic_relocate_24dp,
-            size,
-            this
-        ),
-        SettingsItemMain(
-            resources!!.getString(R.string.settings_about),
-            R.drawable.ic_about_24dp,
-            size,
-            this
+    override fun getSettingsList() {
+        val size = 16 * resources!!.getDisplayMetrics().scaledDensity
+        
+        return listOf(
+            SettingsItemMain(
+                resources!!.getString(R.string.settings_general),
+                R.drawable.ic_general_24dp,
+                size,
+                this
+            ),
+            SettingsItemMain(
+                resources!!.getString(R.string.settings_themes),
+                R.drawable.ic_theme_24dp,
+                size,
+                this
+            ),
+            SettingsItemMain(
+                resources!!.getString(R.string.settings_relocate),
+                R.drawable.ic_relocate_24dp,
+                size,
+                this
+            ),
+            SettingsItemMain(
+                resources!!.getString(R.string.settings_about),
+                R.drawable.ic_about_24dp,
+                size,
+                this
+            )
         )
-    )
+    }
 }

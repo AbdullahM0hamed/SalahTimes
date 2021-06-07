6068c60cdeb8d08c9c91fcb60cd07204470519bb
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2021-06-07 20:23:56 +0100

    Re-design settings

--- app/src/main/kotlin/com/prayer/times/ui/controller/settings/SettingsItemMain.kt
+++ app/src/main/kotlin/com/prayer/times/ui/controller/settings/SettingsItemMain.kt
@@ -1,43 +1,30 @@
 package com.prayer.times.ui.controller.settings
 
-import android.util.TypedValue
-import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import com.mikepenz.fastadapter.FastAdapter
-import com.mikepenz.fastadapter.items.AbstractItem
+import com.mikepenz.fastadapter.binding.AbstractBindingItem
 import com.prayer.times.R
+import com.prayer.times.databinding.SettingsItemBinding
 import com.prayer.times.ui.controller.base.BaseController
 
 data class SettingsItemMain(
     val name: String,
     val icon: Int,
     val controller: BaseController<*>
-) : AbstractItem<SettingsItemMain.ViewHolder>() {
+) : AbstractBindingItem<SettingsItemBinding>() {
 
-    override val type: Int
-        get() = R.id.settings_item_main
+    override val type: Int = R.id.settings_item_main
+    
+    override fun bindView(binding: SettingsItemBinding, payloads: List<Any>) {
+        binding.icon.setImageResource(item.icon)
+        binding.name.text = item.name
+    }
 
-    override val layoutRes: Int
-        get() = R.layout.preference
-
-    override fun getViewHolder(v: View) = ViewHolder(v)
-
-    class ViewHolder(view: View) : FastAdapter.ViewHolder<SettingsItemMain>(view) {
-        var icon: ImageView = view.findViewById(android.R.id.icon)
-        var name: TextView = view.findViewById(android.R.id.title)
-        var summary: TextView = view.findViewById(android.R.id.summary)
-
-        override fun bindView(item: SettingsItemMain, payloads: List<Any>) {
-            summary.visibility = View.GONE
-            icon.setImageResource(item.icon)
-            name.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16f)
-            name.text = item.name
-        }
-
-        override fun unbindView(item: SettingsItemMain) {
-            name.text = null
-        }
+    override fun createBinding(
+        inflater: LayoutInflater,
+        parent: ViewGroup?
+    ): SettingsItemBinding {
+        return SettingsItemBinding.inflate(inflater, parent, false)
     }
 }

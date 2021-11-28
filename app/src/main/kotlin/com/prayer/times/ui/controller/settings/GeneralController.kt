c43371265fbddd0a4d269bafebba43caa434c010
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2021-11-28 15:53:37 +0000

    General settings code I

--- app/src/main/kotlin/com/prayer/times/ui/controller/settings/GeneralController.kt
+++ app/src/main/kotlin/com/prayer/times/ui/controller/settings/GeneralController.kt
@@ -1,28 +1,48 @@
 package com.prayer.times.ui.controller.settings
 
+import android.content.Context
 import com.prayer.times.R
 import com.prayer.times.ui.controller.base.SettingsController
 
 class GeneralController : SettingsController() {
 
     override fun getHeaderTitle() = resources!!.getString(R.string.settings_general)
-    override fun getSettingsList() = listOf(
-        GeneralItem(
-            resources!!.getString(R.string.language_title),
-            resources!!.getString(R.string.language_default),
-            { android.widget.Toast.makeText(activity as android.content.Context, "Test", 5).show() }
-        ),
-        GeneralItem(
-            resources!!.getString(R.string.calculation_method),
-            resources!!.getString(R.string.moonsighting)
-        ),
-        GeneralItem(
-            resources!!.getString(R.string.madhab),
-            resources!!.getString(R.string.shafi)
-        ),
-        GeneralItem(
-            resources!!.getString(R.string.time_format),
-            resources!!.getString(R.string.military_time)
+    override fun getSettingsList(): List<GeneralItem> {
+        val context = activity as Context
+    
+        return listOf(
+            GeneralItem(
+                context,
+                resources!!.getString(R.string.language_title),
+                resources!!.getString(R.string.language_summary),
+                "language"
+                resources!!.getString(R.string.language_default),
+                resources!!.getStringArray(R.array.languages)
+            ),
+            GeneralItem(
+                context,
+                resources!!.getString(R.string.calculation_method),
+                resources!!.getString(R.string.calculation_summary),
+                "calculation_method"
+                resources!!.getString(R.string.moonsighting),
+                resources!!.getStringArray(R.array.madaahib)
+            ),
+            GeneralItem(
+                context,
+                resources!!.getString(R.string.madhab),
+                resources!!.getString(R.string.madhab_summary),
+                "madhab",
+                resources!!.getString(R.string.shafi),
+                resources!!.getStringArray(R.array.madaahib)
+            ),
+            GeneralItem(
+                context,
+                resources!!.getString(R.string.time_format),
+                resources!!.getString(R.string.time_format_summary),
+                "time_format",
+                resources!!.getString(R.string.military_time),
+                resources!!.getStringArray(R.array.timeFormats)
+            )
         )
-    )
+    }
 }
--- app/src/main/kotlin/com/prayer/times/ui/controller/settings/GeneralItem.kt
+++ app/src/main/kotlin/com/prayer/times/ui/controller/settings/GeneralItem.kt
@@ -1,28 +1,50 @@
 package com.prayer.times.ui.controller.settings
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import com.mikepenz.fastadapter.FastAdapter
 import com.mikepenz.fastadapter.binding.AbstractBindingItem
 import com.prayer.times.R
 import com.prayer.times.databinding.GeneralItemBinding
+import com.prayer.times.preference.PreferencesHelper
 import com.prayer.times.ui.controller.base.BaseController
 
 data class GeneralItem(
+    val context: Context,
     val name: String,
     val desc: String,
-    val onClick: Function0<Unit> = {}
+    val key: String,
+    val default: String,
+    val options: Array<String>
 ) : AbstractBindingItem<GeneralItemBinding>() {
 
-    override val type: Int = R.id.general_item_main
+    override val type: Int = R.id.general_item_mai
+    val helper = PreferencesHelper(context)
     
     override fun bindView(
         binding: GeneralItemBinding,
         payloads: List<Any>
     ) {
-        binding.root.setOnClickListener { onClick() }
         binding.title.text = name
-        binding.summary.text = desc
+        binding.summary.text = default
+        binding.root.setOnClickListener {
+            val builder = AlertDialog.Builder(context)
+            builder.setTitle(name)
+            builder.setMessage(desc)
+            builder.setSingleChoiceItems(
+                options,
+                options.indexOf(getValue()).let { if (it == -1) default else it }
+            )
+            builder.setPositiveButton(R.string.ok) { dialog, which ->
+                val position = (dialog as AlertDialog).listView.checkedItemPosition
+                helper.putString(options[position])
+            }
+
+            val dialog = builder.create()
+            dialog.show()
+        }
     }
 
     override fun createBinding(
@@ -30,5 +52,9 @@ data class GeneralItem(
         parent: ViewGroup?
     ): GeneralItemBinding {
         return GeneralItemBinding.inflate(inflater, parent, false)
+    }
+
+    fun getValue(): String {
+        return helper.getString(key) ?: default
     }
 }
